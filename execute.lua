94ed28ae4ce252af1a6b7de1c0dacc105c75a247
//s <82622860+Kilka21@users.noreply.github.com>
2023-08-28 17:49:38 +1000

    Create execute.lua

---
+++ execute.lua
@@ -0,0 +1,9 @@
+--RUS: 
+_G.Users = {}
+_G.Prefix = "/"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kilka21/PlayersChecks/main/PlayersChecks%5BRUS%5D.lua")()
+
+ENG:
+_G.Users = {} 
+_G.Prefix = "/"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kilka21/PlayersChecks/main/PlayersCheck%5BENG%5D.lua")()
